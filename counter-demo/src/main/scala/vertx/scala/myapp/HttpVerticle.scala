0129900f514372ca70348feb9ba222d95e5a12c5
Jochen Mader <pflanzenmoerder@gmail.com>
2018-04-19 21:36:07 +0200

    Some more htpp-tests

--- counter-demo/src/main/scala/vertx/scala/myapp/HttpVerticle.scala
+++ counter-demo/src/main/scala/vertx/scala/myapp/HttpVerticle.scala
@@ -9,19 +9,41 @@ import scala.util.{Failure, Success}
 class HttpVerticle extends ScalaVerticle {
 
   override def startFuture(): Future[_] = {
+
+    val host = config.getString("host")
+
+    val client = vertx.createHttpClient()
+
+    var local_counter = 0
+
     vertx
       .sharedData()
       .getCounterFuture("requestCounter")
       .map(counter => {
         val router = Router.router(vertx)
-        router.get("/count").handler(req => {
+
+        router.get("/count_distributed").handler(req => {
             counter.incrementAndGetFuture()
             req.response().end("counted")
-          }).failureHandler(_ => ())
-        router.get("/value").handler(req => counter.getFuture().onComplete{
+          })
+
+
+        router.get("/count_from_other").handler(req => {
+          local_counter = local_counter + 1
+          req.response().end("counted")
+        })
+
+        router.get("/count_http").handler(req => {
+            req.response().end("counted")
+            client.get(8666, host, "/count_from_other").handler(resp => ()).end()
+          })
+
+        router.get("/value_distributed").handler(req => counter.getFuture().onComplete{
           case Success(value) =>  req.response().end(value.toString)
           case Failure(t)     =>  req.fail(t)
-        }).failureHandler(_ => ())
+        })
+
+        router.get("/value_local").handler(req => req.response().end(local_counter.toString))
 
         vertx
           .createHttpServer()
